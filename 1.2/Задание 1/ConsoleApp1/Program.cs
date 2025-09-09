c36817ed8ec1013721cabf1cfcee6c5d13f0f3e5
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-09-09 11:35:56 +0300

    feat: добавлены задания из кр1

--- 1.2/Задание 1/ConsoleApp1/Program.cs
+++ 1.2/Задание 1/ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день, с которого будет начинаться месяц (1 Пн - 7 Вс): ");
+int start = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите день, который нужно проверить: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+string[] daysOfWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+
+int dayOfWeekIndex = (start - 1 + day - 1) % 7;
+string dayOfWeek = daysOfWeek[dayOfWeekIndex];
+
+Console.WriteLine($"День {day} месяца - это {dayOfWeek}");
+
+bool isWeekend = dayOfWeek == "Сб" || dayOfWeek == "Вс";
+bool isHoliday = (day >= 1 && day <= 5) || (day >= 8 && day <= 10);
+
+if (isWeekend || isHoliday)
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
+}
---
+++ 1.2/Задание 2/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n > 150000)
+{
+    Console.Write("Банкомат не может выдать более 150000 рублей за одну операцию, попробуйте другую сумму. N: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] vals = [5000, 2000, 1000, 500, 200, 100];
+
+foreach (var val in vals)
+{
+    int c = n / val;
+    if (c > 0)
+    {
+        n %= val;
+        Console.WriteLine($"{val}: {c}");
+    }
+}
