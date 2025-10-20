583735fea489aad62b015abd40285a2665f97ff1
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-10-20 21:50:29 +0300

    feat: добавил 2.1 2

---
+++ 2.1/Задание 2/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.Write("Введите номер билета: ");
+    string number = Console.ReadLine();
+
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (i < number.Length / 2)
+        {
+            sum1 += Convert.ToInt32(number[i]);
+        }
+        else
+        {
+            sum2 += Convert.ToInt32(number[i]);
+        }
+    }
+
+    Console.WriteLine(sum1 == sum2);
+}
