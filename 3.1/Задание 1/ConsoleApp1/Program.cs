ab76af1f668634325ea104faeaf3cf4c7ec40861
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-11-11 11:43:36 +0300

    feat: 3.1 1

---
+++ 3.1/Задание 1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Reverse(n, 0));
+
+int Reverse(int initial, int result)
+{
+    if(initial > 0)
+    {
+        result = (result * 10) + (initial % 10);
+        return Reverse(initial / 10, result);
+    }
+    else
+    {
+        return result;
+    }
+}
+
