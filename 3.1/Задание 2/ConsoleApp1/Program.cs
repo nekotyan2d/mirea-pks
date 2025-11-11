5ae39f75be9d52e49ebdee2f539d3a2a84f07b54
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-11-11 11:52:54 +0300

    feat: 3.1 2

---
+++ 3.1/Задание 2/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return A(m - 1, 1);
+    }
+    else
+    {
+        return A(m - 1, A(m, n - 1));
+    }
+}
+
+Console.Write("m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A({m}, {n}) = {A(m, n)}");
